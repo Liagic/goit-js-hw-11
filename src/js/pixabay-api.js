eeb6f03f3d172e4b3746bf1d3c98875b40ef9506
liagic <leon_max@i.ua>
2026-02-20 21:25:48 +0200

    added api get method, error handler, render page

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const apiKey = '54728905-47b91fba438408292028d34aa';
+export default function getImagesByQuery(query) {
+  return axios.get('', {
+    params: {
+      key: apiKey,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  });
+}
---
+++ src/js/render-functions.js
@@ -0,0 +1,53 @@
+export const gallery = document.querySelector('.gallery');
+export function createGallery(images) {
+  const items = images
+    .map(
+      ({
+        webformatURL = '',
+        largeImageURL = '',
+        tags = '',
+        likes = '0',
+        views = '0',
+        comments = '0',
+        downloads = '0',
+      }) => {
+        if (webformatURL.trim() != '' || largeImageURL.trim() != '') {
+          return `<li class="gallery-item">
+  <a class="gallery-link" href="${largeImageURL}">
+    <img
+      class="gallery-image"
+      src="${webformatURL}"
+      data-source="${largeImageURL}"
+      alt="${tags}"
+      width= 360px height=200px;
+    />
+    <ul class="gallery-additional-info">
+    <li class="additional-info-text"><span class="additional-info-label">Likes</span>
+    <span class="additional-info-value">${likes}</span></li>
+    <li class="additional-info-text"><span class="additional-info-label">Views</span>
+    <span class="additional-info-value">${views}</span></li>
+    <li class="additional-info-text"><span class="additional-info-label">Comments</span>
+    <span class="additional-info-value">${comments}</span></li>
+    <li class="additional-info-text"><span class="additional-info-label">Downloads</span>
+    <span class="additional-info-value">${downloads}</span></li>
+    </ul>
+  </a>
+</li>`;
+        }
+      }
+    )
+    .join('');
+  return items;
+}
+
+export function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+export function showLoader() {
+  gallery.classList.add('loader');
+}
+
+export function hideLoader() {
+  gallery.classList.remove('loader');
+}
